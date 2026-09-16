a641360897b975ec3704f30cf13d14b2d2aeb648
danielbrownmsm <daniel.brown@msmrockets.org>
2026-09-16 10:49:37 -0500

    Make the '/link' command.
    Sends the bot to open a DM with you and ask for the Canvas API token for increased security. Since database is not set up yet as of now it just prints out the token.

--- bot/bot.py
+++ bot/bot.py
@@ -40,3 +40,37 @@ async def on_ready():
     print(f"Logged in as {client.user} (ID: {client.user.id})")
     print("------")
 
+
+# Link command to get and store canvas API token
+@client.tree.command()
+async def link(interaction: discord.Interaction):
+    """Securely link your Canvas and Discord accounts."""
+
+    # Ask for API token in DMs so there is no risk of it being exposed in a server
+    if interaction.user.dm_channel is None:
+        await interaction.user.create_dm()
+
+    # TODO: might need a try/catch around this if not open DMs?
+    await interaction.user.dm_channel.send(
+        f"""TODO: insert instructions for locating your canvas API token (with screenshots preferably) here.
+        Please respond to this message with your API token."""
+    )
+
+
+# !!! THIS IS ONLY FOR INTERACTING IN DMS !!!
+# !!! EVERYTHING ELSE SHOULD BE HANDLED THROUGH A SLASH COMMAND !!!
+@client.event
+async def on_message(message: discord.Message):
+    # first check and see if we are triggering on ourself
+    if message.author.id == client.user.id:
+        return
+
+    if message.channel.type == discord.DMChannel:
+        # this is someone trying to give us their API token, so store it or something
+
+        print(f"Got API token: {message.content}")
+
+        pass  # TODO
+
+    # otherwise, do nothing
+    # FIXME do we want to have them able to ask for help in DMs as well?
